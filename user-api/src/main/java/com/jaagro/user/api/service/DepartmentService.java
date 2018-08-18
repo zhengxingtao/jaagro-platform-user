6a9b948b298a5c0b647c2843e5d51fe78ad48b66
liqiangping <1012916300@qq.com>
2018-08-18 15:10:35 +0800

    部门的添加修改

--- user-api/src/main/java/com/jaagro/user/api/service/DepartmentService.java
+++ user-api/src/main/java/com/jaagro/user/api/service/DepartmentService.java
@@ -1,7 +1,24 @@
 package com.jaagro.user.api.service;
 
+import com.jaagro.user.api.dto.request.department.CreateDepartmentDto;
+import com.jaagro.user.api.dto.request.department.UpdateDepartmentDto;
+
+import java.util.Map;
+
 /**
  * @author tony
  */
 public interface DepartmentService {
+    /**
+     * 创建部门
+     * @param dto
+     * @return
+     */
+    Map<String, Object> createDepartment(CreateDepartmentDto dto);
+    /**
+     * 修改部门
+     * @param dto
+     * @return
+     */
+    Map<String, Object> updateById(UpdateDepartmentDto dto);
 }
