74b6cc1164ab36cab34068fcc2047fdecebf3a8b
liqiangping <1012916300@qq.com>
2018-08-18 18:26:10 +0800

    部门的单表增删改查多条件查询

--- user-api/src/main/java/com/jaagro/user/api/service/DepartmentService.java
+++ user-api/src/main/java/com/jaagro/user/api/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.jaagro.user.api.service;
 
 import com.jaagro.user.api.dto.request.department.CreateDepartmentDto;
+import com.jaagro.user.api.dto.request.department.ListDepartmentCriteriaDto;
 import com.jaagro.user.api.dto.request.department.UpdateDepartmentDto;
 
 import java.util.Map;
@@ -21,4 +22,25 @@ public interface DepartmentService {
      * @return
      */
     Map<String, Object> updateById(UpdateDepartmentDto dto);
+    /**
+     * 获取单条
+     *
+     * @param id
+     * @return
+     */
+    Map<String, Object> getById(Long id);
+    /**
+     * 逻辑删除部门
+     *
+     * @param id
+     * @return
+     */
+    Map<String, Object> disableDepartment(Long id);
+    /**
+     * 分页获取list，注意criteria查询条件
+     *
+     * @param dto
+     * @return
+     */
+    Map<String, Object> listByCriteria(ListDepartmentCriteriaDto dto);
 }
--- user-biz/src/main/java/com/jaagro/user/biz/service/impl/DepartmentServiceImpl.java
+++ user-biz/src/main/java/com/jaagro/user/biz/service/impl/DepartmentServiceImpl.java
@@ -1,7 +1,11 @@
 package com.jaagro.user.biz.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jaagro.user.api.dto.request.department.CreateDepartmentDto;
+import com.jaagro.user.api.dto.request.department.ListDepartmentCriteriaDto;
 import com.jaagro.user.api.dto.request.department.UpdateDepartmentDto;
+import com.jaagro.user.api.dto.response.department.DepartmentReturnDto;
 import com.jaagro.user.api.service.DepartmentService;
 import com.jaagro.user.biz.entity.Department;
 import com.jaagro.user.biz.mapper.DepartmentMapper;
@@ -10,9 +14,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import utils.ResponseStatusCode;
 import utils.ServiceResult;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -23,8 +29,10 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Autowired
     private UserMapper userMapper;
+
     /**
      * 创建部门
+     *
      * @param dto
      * @return
      */
@@ -32,8 +40,8 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Override
     public Map<String, Object> createDepartment(CreateDepartmentDto dto) {
         //创建部门对象
-        Department department=new Department();
-        BeanUtils.copyProperties(dto,department);
+        Department department = new Department();
+        BeanUtils.copyProperties(dto, department);
         department
                 .setCreateTime(new Date())
                 .setEnabled(true);
@@ -43,17 +51,69 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     /**
      * 修改部门
+     *
      * @param dto
      * @return
      */
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public Map<String, Object> updateById(UpdateDepartmentDto dto) {
         //创建部门对象
-        Department department=new Department();
-        BeanUtils.copyProperties(dto,department);
+        Department department = new Department();
+        BeanUtils.copyProperties(dto, department);
         department
                 .setCreateTime(new Date());
         departmentMapper.updateByPrimaryKeySelective(department);
         return ServiceResult.toResult("修改部门成功");
+    }
+
+    /**
+     * 查询单个部门
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public Map<String, Object> getById(Long id) {
+        if (departmentMapper.selectByPrimaryKey(id) == null) {
+            return ServiceResult.error(ResponseStatusCode.ID_VALUE_ERROR.getCode(), "id: " + id + "不存在");
+        }
+        return ServiceResult.toResult(departmentMapper.getById(id));
+    }
+
+    /**
+     * 逻辑删除部门
+     *
+     * @param id
+     * @return
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public Map<String, Object> disableDepartment(Long id) {
+        //创建部门Dto返回的对象
+        DepartmentReturnDto departmentDto = departmentMapper.getById(id);
+        //创建部门对象
+        Department department = new Department();
+        BeanUtils.copyProperties(departmentDto, department);
+        if (department != null) {
+            //逻辑删除部门
+            department.setEnabled(false);
+            departmentMapper.updateByPrimaryKeySelective(department);
+        }
+        return ServiceResult.toResult("部门删除成功");
+    }
+
+    /**
+     * 分页获取list，注意criteria查询条件
+     *
+     * @param dto
+     * @return
+     */
+    @Override
+
+    public Map<String, Object> listByCriteria(ListDepartmentCriteriaDto dto) {
+        PageHelper.startPage(dto.getPageNum(), dto.getPageSize());
+        List<DepartmentReturnDto> departmentReturnDtos = this.departmentMapper.getByCriteriDto(dto);
+        return ServiceResult.toResult(new PageInfo<>(departmentReturnDtos));
     }
 }
