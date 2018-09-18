4e7f7a6524050dfe9273abd1fc571bab95f12721
zhengxingtao <zheng_xigntao@126.com>
2018-09-18 11:17:59 +0800

    更新driver

--- user-biz/src/main/java/com/jaagro/user/biz/mapper/DriverMapper.java
+++ user-biz/src/main/java/com/jaagro/user/biz/mapper/DriverMapper.java
@@ -5,37 +5,37 @@ import com.jaagro.user.biz.entity.Driver;
 public interface DriverMapper {
     /**
      *
-     * @mbggenerated 2018-08-22
+     * @mbggenerated 2018-09-18
      */
     int deleteByPrimaryKey(Integer id);
 
     /**
      *
-     * @mbggenerated 2018-08-22
+     * @mbggenerated 2018-09-18
      */
     int insert(Driver record);
 
     /**
      *
-     * @mbggenerated 2018-08-22
+     * @mbggenerated 2018-09-18
      */
     int insertSelective(Driver record);
 
     /**
      *
-     * @mbggenerated 2018-08-22
+     * @mbggenerated 2018-09-18
      */
     Driver selectByPrimaryKey(Integer id);
 
     /**
      *
-     * @mbggenerated 2018-08-22
+     * @mbggenerated 2018-09-18
      */
     int updateByPrimaryKeySelective(Driver record);
 
     /**
      *
-     * @mbggenerated 2018-08-22
+     * @mbggenerated 2018-09-18
      */
     int updateByPrimaryKey(Driver record);
 }
