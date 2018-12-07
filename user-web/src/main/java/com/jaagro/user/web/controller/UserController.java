bab34b4124543be986d1b3b9fd01cf0bd8eaa0f2
zhengxingtao <zheng_xigntao@126.com>
2018-12-07 11:07:44 +0800

    globalUser2

--- user-web/src/main/java/com/jaagro/user/web/controller/UserController.java
+++ user-web/src/main/java/com/jaagro/user/web/controller/UserController.java
@@ -2,10 +2,12 @@ package com.jaagro.user.web.controller;
 
 import com.jaagro.constant.UserInfo;
 import com.jaagro.user.api.service.UserService;
+import com.jaagro.utils.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,10 @@ public class UserController {
             return null;
         }
         return userService.listUserInfo(userIdList, userType);
+    }
+
+    @GetMapping("/getGlobalUser/{userId}")
+    public BaseResponse<UserInfo> getGlobalUser(@PathVariable("userId") int userId){
+        return BaseResponse.successInstance(userService.getGlobalUser(userId));
     }
 }
