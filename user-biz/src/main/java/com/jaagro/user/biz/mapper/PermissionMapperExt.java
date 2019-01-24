a95268027733f48d44512bb95a8b2ac5cb7a1fb4
zhengxingtao <zheng_xigntao@126.com>
2019-01-24 09:56:21 +0800

    调整permission结构，将int的id改成string

--- user-biz/src/main/java/com/jaagro/user/biz/mapper/PermissionMapperExt.java
+++ user-biz/src/main/java/com/jaagro/user/biz/mapper/PermissionMapperExt.java
@@ -1,25 +1,8 @@
 package com.jaagro.user.biz.mapper;
 
-import com.jaagro.user.biz.entity.Permission;
-
-import java.util.List;
-
 /**
  * @author tony
+ * @date 2019-01-24
  */
 public interface PermissionMapperExt extends PermissionMapper {
-    /**
-     * 根据权限编号查询
-     *
-     * @param accessNumber
-     * @return
-     */
-    List<Permission> getByNumber(String accessNumber);
-
-    /**
-     * 获取全部的权限
-     *
-     * @return
-     */
-    List<Permission> listAll();
 }
